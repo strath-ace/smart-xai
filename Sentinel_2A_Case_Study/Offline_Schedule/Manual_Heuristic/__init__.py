46b03afa337b69c1ff934334a295ec94ff27ee64
Chiyuri <72197444+chiyuri@users.noreply.github.com>
2021-11-05 16:54:40 +0000

    Create __init__.py

---
+++ Sentinel_2A_Case_Study/Offline_Schedule/Manual_Heuristic/__init__.py
@@ -0,0 +1,7 @@
+import os
+# from .Land_visibility import *
+# from .ground_station_access import *
+# from .Shade_exposure import *
+
+# engine = create_engine(os.environ['DATABASE_URL'])
+# Session = sessionmaker(bind=engine)
