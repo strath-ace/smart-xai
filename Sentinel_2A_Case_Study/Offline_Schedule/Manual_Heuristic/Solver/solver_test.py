62cb104b55a09261c72e020dd839db7b34685f05
Chiyuri <72197444+chiyuri@users.noreply.github.com>
2021-11-08 11:14:22 +0000

    updates after file moved

--- Sentinel_2A_Case_Study/Offline_Schedule/Manual_Heuristic/Solver/solver_test.py
+++ Sentinel_2A_Case_Study/Offline_Schedule/Manual_Heuristic/Solver/solver_test.py
@@ -1,14 +1,14 @@
 from CPModel_with_Hint import CPModel_data
 from CPSolver import CP_solver
 from Sentinel_2A_Case_Study.Environment.environment_data_to_solver import environment_data
-from Sentinel_2A_Case_Study.Offline_Schedule.Manual_Heuristic.Manually_created_schedule.Manual_binary_data import manual_data_retrieved
+from Sentinel_2A_Case_Study.Offline_Schedule.Manual_Heuristic.Manually_created_schedule.Manual_binary_data import manual_binary_data
 
 day = 1
 month = 'Dec'
 year = 2020
 country = 'All'
 time_interval = 5
-interval = 3000
+interval = 5000
 # onboard memory is 80% of total memory
 onboard_mem = int(0.8 * 24 * 10 ** 5)
 #  memory required per image
@@ -21,10 +21,10 @@ process_im_mem = 50 * time_interval
 
 filename = '../../Results/Day '
 path = '../../../Environment/'
-path1 = '../../Results/Day ' + str(day)
+path1 = '../../Results/Day '
 
 country_data_list, gnd_data_list, day_data_list = environment_data(path,time_interval, day, month, year, country)
-mem_data_list = manual_data_retrieved(path1, time_interval, day, month)[0]
+mem_data_list = manual_binary_data(path1, time_interval, day, month)[0]
 
 i = 0
 horizon = len(country_data_list)
