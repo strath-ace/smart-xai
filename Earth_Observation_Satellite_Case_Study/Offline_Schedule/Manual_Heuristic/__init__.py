0180146e32fd13a8c93f6dd67434de50bbe1624f
Chiyuri <72197444+chiyuri@users.noreply.github.com>
2022-06-13 23:28:23 +0100

    init.py codes commented

--- Earth_Observation_Satellite_Case_Study/Offline_Schedule/Manual_Heuristic/__init__.py
+++ Earth_Observation_Satellite_Case_Study/Offline_Schedule/Manual_Heuristic/__init__.py
@@ -1,7 +1,2 @@
-import os
-# from .Land_visibility import *
-# from .ground_station_access import *
-# from .Shade_exposure import *
 
-# engine = create_engine(os.environ['DATABASE_URL'])
-# Session = sessionmaker(bind=engine)
+import os
--- Earth_Observation_Satellite_Case_Study/Offline_Schedule/Manual_Heuristic/graph_plot_code/__init__.py
+++ Earth_Observation_Satellite_Case_Study/Offline_Schedule/Manual_Heuristic/graph_plot_code/__init__.py
@@ -1,3 +1,4 @@
+# This file is created to call the functions from existing files
 from Earth_Observation_Satellite_Case_Study.Offline_Schedule.Manual_Heuristic.graph_plot_code.manual_memory_plot import *
 from Earth_Observation_Satellite_Case_Study.Offline_Schedule.Manual_Heuristic.graph_plot_code.Solver_plot import *
 from Earth_Observation_Satellite_Case_Study.Offline_Schedule.Manual_Heuristic.graph_plot_code.gantt_plot import *
